e7e6b2887085d62913d73f5e9ff861262cf86846
YURII D <gkhelloworld@gmail.com>
2023-02-20 13:49:19 +0200

    update

---
+++ packages/sources-icons-react/test.spec.js
@@ -0,0 +1,27 @@
+import { render } from "@testing-library/react"
+import { IconUaNbu } from "./src/icons.js"
+import React from "react"
+import renderer from 'react-test-renderer'
+
+describe("React Icon component", () => {
+  test("should render icon component", () => {
+    const { container } = render(<IconUaNbu/>)
+    expect(container.getElementsByTagName("svg").length).toBeGreaterThan(0)
+  })
+
+  test("should update svg attributes when there are props passed to the component", () => {
+    const { container } = render(<IconUaNbu size={48} color={"red"} strokeWidth={4}/>)
+
+    const svg = container.getElementsByTagName("svg")[0]
+
+    expect(svg.getAttribute("width")).toBe("48")
+    expect(svg.getAttribute("stroke")).toBe("red")
+    expect(svg.getAttribute("stroke-width")).toBe("4")
+  })
+
+  // Jest creates separate file to store snapshots
+  test("should match snapshot", () => {
+    const icon = renderer.create(<IconUaNbu/>).toJSON()
+    expect(icon).toMatchSnapshot()
+  })
+})
