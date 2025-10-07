031f3f2449a8d24dfe832f1779ad1938a7f5bf67
YURII D. <gkhelloworld@gmail.com>
2025-10-08 02:28:27 +0300

    fix: use git diff with latest tag instead of staged changes

--- .build/changelog-release.mjs
+++ .build/changelog-release.mjs
@@ -1,25 +1,39 @@
 import cp from 'child_process'
 import { printChangelog } from './helpers.mjs'
 
-// Use git diff --staged to check staged changes for release
-cp.exec('git diff --staged --name-status src/_icons', function(err, ret) {
-  let newIcons = [], modifiedIcons = [], renamedIcons = []
+// Get the latest version tag
+cp.exec('git describe --tags --abbrev=0', function(err, latestTag) {
+  if (err) {
+    // If no tags exist, compare with first commit
+    latestTag = ''
+  } else {
+    latestTag = latestTag.trim()
+  }
 
-  ret.replace(/A\s+src\/_icons\/([a-z0-9-]+)\.svg/g, function(m, fileName) {
-    newIcons.push(fileName)
+  // Compare current HEAD with the latest tag to find changes
+  const command = latestTag 
+    ? `git diff ${latestTag} HEAD --name-status src/_icons`
+    : 'git log --name-status --pretty=format: src/_icons'
+
+  cp.exec(command, function(err, ret) {
+    let newIcons = [], modifiedIcons = [], renamedIcons = []
+
+    ret.replace(/A\s+src\/_icons\/([a-z0-9-]+)\.svg/g, function(m, fileName) {
+      newIcons.push(fileName)
+    })
+
+    ret.replace(/M\s+src\/_icons\/([a-z0-9-]+)\.svg/g, function(m, fileName) {
+      modifiedIcons.push(fileName)
+    })
+
+    ret.replace(/R[0-9]+\s+src\/_icons\/([a-z0-9-]+)\.svg\s+src\/_icons\/([a-z0-9-]+).svg/g, function(m, fileNameBefore, fileNameAfter) {
+      renamedIcons.push([fileNameBefore, fileNameAfter])
+    })
+
+    modifiedIcons = modifiedIcons.filter(function(el) {
+      return newIcons.indexOf(el) < 0
+    })
+
+    printChangelog(newIcons, modifiedIcons, renamedIcons)
   })
-
-  ret.replace(/M\s+src\/_icons\/([a-z0-9-]+)\.svg/g, function(m, fileName) {
-    modifiedIcons.push(fileName)
-  })
-
-  ret.replace(/R[0-9]+\s+src\/_icons\/([a-z0-9-]+)\.svg\s+src\/_icons\/([a-z0-9-]+).svg/g, function(m, fileNameBefore, fileNameAfter) {
-    renamedIcons.push([fileNameBefore, fileNameAfter])
-  })
-
-  modifiedIcons = modifiedIcons.filter(function(el) {
-    return newIcons.indexOf(el) < 0
-  })
-
-  printChangelog(newIcons, modifiedIcons, renamedIcons)
 })
